0fb3f284382c67dc85406d1d7c075d97136bfb5b
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-01-25 17:40:13 -0500

    added tests and ability to say hello to any input

--- code.js
+++ code.js
@@ -3,10 +3,13 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(input){
-    if (input === "Alex"){
-        return "Hello, Alex!";
-    } else {
-        return "Hello, Jane!";
-    }
+function sayHello(input) {
+    return ("Hello, " + input + "!");
+    // if (input === "Alex") {
+    //     return "Hello, Alex!";
+    // } else if (input === "Pat") {
+    //  return "Hello, Pat!"
+    // } else {
+    //     return "Hello, Jane!";
+    // }
 }
--- tests.js
+++ tests.js
@@ -21,10 +21,13 @@ describe('sayHello',function (){
     it('should return a string when called', function () {
         expect(typeof sayHello()).toBe("string");
     });
-    it('should should return the string "Hello, Jane!" when executed', function () {
-        expect(sayHello()).toBe("Hello, Jane!");
+    it('should return the string "Hello, Jane!" when executed', function () {
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
-    it('should should return the string "Hello, Alex!" when executed', function (){
+    it('should return the string "Hello, Alex!" when executed', function (){
         expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
+    it('should return the string "Hello, Pat!" when executed', function (){
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    })
 });
