27554381e71d937f48113d732e144ffbfe9af863
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-01-25 17:19:04 -0500

    exercise four: another small, simple test

--- code.js
+++ code.js
@@ -3,6 +3,10 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(){
-    return "Hello, Jane!";
+function sayHello(input){
+    if (input === "Alex"){
+        return "Hello, Alex!";
+    } else {
+        return "Hello, Jane!";
+    }
 }
--- tests.js
+++ tests.js
@@ -18,10 +18,13 @@ describe('sayHello',function (){
     it('should be a defined function',function (){
         expect(typeof sayHello).toBe('function');
     });
-    it('should return a string', function () {
+    it('should return a string when called', function () {
         expect(typeof sayHello()).toBe("string");
     });
-    it('should should return the string "Hello, Jane!"', function () {
+    it('should should return the string "Hello, Jane!" when executed', function () {
         expect(sayHello()).toBe("Hello, Jane!");
+    });
+    it('should should return the string "Hello, Alex!" when executed', function (){
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
 });
